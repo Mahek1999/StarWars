577ad9b94f12e60116fe1f1b9be78519a05db0e1
Mahek1999 <s99.mahek@gmail.com>
2023-07-10 21:13:58 +0530

    Dropdown Change

--- src/Components/PlanetSelector.js
+++ src/Components/PlanetSelector.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "../App.css"
 
 const PlanetSelector = ({ onPlanetSelect }) => {
   const [planets, setPlanets] = useState([]);
@@ -36,7 +37,7 @@ const PlanetSelector = ({ onPlanetSelect }) => {
   return (
     <div>
       <label htmlFor="planet" className="select"> Please Select a planet:</label>
-      <select id="planet" value={selectedPlanet} onChange={handlePlanetChange}>
+      <select id="planet" value={selectedPlanet} className="planet" onChange={handlePlanetChange}>
         <option value=""></option>
         {planets.map((planet) => (
           <option key={planet.name} value={planet.name}>
